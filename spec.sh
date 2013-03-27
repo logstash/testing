ba585c9ef12a653e864b96bf5bb7b3c7f828f597
Jordan Sissel <jls@semicomplete.com>
2013-03-26 22:18:17 -0700

    - move stuff

--- spec.sh
+++ spec.sh
@@ -1,8 +1,14 @@
 . ./lib.rc
-quiet vagrant up
+export FACTER_roles=redis
+note "==> Getting a VM"
+quiet vagrant up --no-provision
+note "==> Provisioning..."
+quiet vagrant provision
 vagrant ssh <<SCRIPT
 `cat ./lib.rc`
 cd /app/logstash/logstash || exit 1
+note "==> Preparing logstash"
 quiet sudo -u logstash make vendor/bundle || exit 1
+note "==> Running tests: $*"
 exec sudo -u logstash env USE_JRUBY=1 bin/logstash rspec $*
 SCRIPT
