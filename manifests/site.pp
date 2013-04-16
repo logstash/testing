6cfd1f7892cac1875d6173d693f8a3d11ebe8cf6
Jordan Sissel <jls@semicomplete.com>
2013-04-15 19:21:17 -0700

    - provide my own apt sources since the default ones are insanely slow

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,39 @@
 stage {
   "upgrade": before => Stage["main"];
+  #"apt-init": before => Stage["upgrade"];
 }
 
 class {
-  "apt::upgrade": stage => "upgrade";
+  "apt": purge_sources_list => true;
+    #stage => "upgrade";
+  "apt::upgrade": ; #stage => "upgrade";
   "os": ;
   "vagrant::guest": ;
   "logstash": 
     revision => $logstash_revision;
+}
+
+#Apt::Source {
+  #stage => "upgrade"
+#}
+
+apt::source {
+  "$lsbdistcodename":
+    release => $lsbdistcodename,
+    location => "http://mirror.math.ucdavis.edu/ubuntu/",
+    repos => "main restricted universe multiverse";
+  "$lsbdistcodename-updates":
+    release => "$lsbdistcodename-updates",
+    location => "http://mirror.math.ucdavis.edu/ubuntu/",
+    repos => "main restricted universe multiverse";
+  "$lsbdistcodename-backports":
+    location => "http://mirror.math.ucdavis.edu/ubuntu/",
+    release => "$lsbdistcodename-backports",
+    repos => "main restricted universe multiverse";
+  "$lsbdistcodename-security":
+    location => "http://security.ubuntu.com/ubuntu",
+    release => "$lsbdistcodename-security",
+    repos => "main restricted universe multiverse";
 }
 
 $roles_list = split($roles, ",")
